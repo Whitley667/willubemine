4b0d28097e8022d307181add7b97a1c6d0110be6
Whitley667 <ayansultanov@gmail.com>
2024-02-13 23:20:20 +0100

    setup gh-pages

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+export default function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen -mt-16">
+      {yesPressed ? (
+        <>
+        <img src="https://media1.tenor.com/m/arqlNu8gyJYAAAAC/cat-cat-jumping.gif" />
+        <div className="text-4xl font-bold my-4">Ok yay!!!</div>
+        </>
+      ) : (
+        <>
+          <img className="h-[200px]" src="https://i.pinimg.com/originals/fa/a1/77/faa1779c4b9f0770e5f215e78f099271.gif" />
+          <h1 className="text-4xl my-4">Will you be my Valentine?</h1>
+          <div>
+            <button
+              className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
